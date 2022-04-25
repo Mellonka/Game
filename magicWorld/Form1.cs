e6ed7f221ed41260e841e33a2b826942ac5eae83
Mellonka <facemellon@gmail.com>
2022-04-26 02:03:55 +0500

    Initial

---
+++ magicWorld/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace magicWorld
+{
+    public partial class Form1 : Form
+    {
+        public Image spriteHero;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Size = new Size(960, 540);
+            Init();
+        }
+
+        void Init()
+        {
+            spriteHero = new Bitmap(Path.Combine(new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.FullName.ToString(), "Sprites\\Wizard.png"));
+
+            var player = new Entity(0, 0, spriteHero);
+
+            Paint += OnPaint;
+            Invalidate();
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            var graphics = e.Graphics;
+
+            graphics.DrawImage(spriteHero, new Point(100, 100));
+
+        }
+    }
+
+
+    
+}
---
+++ magicWorld/Models/Entity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace magicWorld
+{
+    public class Entity
+    {
+        public int posX;
+        public int posY;
+
+        private Image sprite;
+
+        public Entity(int posX, int posY, Image sprite)
+        {
+            this.posX = posX;
+            this.posY = posY;
+            this.sprite = sprite;
+
+        }
+
+        public void Move(int dX, int dY)
+        {
+            posX += dX;
+            posY += dY;
+        }
+
+    }
+}
